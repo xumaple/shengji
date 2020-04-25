a680e72451a7fc0e7727db27f114161238c29dac
Maple Xu <maplexu2010@gmail.com>
2020-04-25 14:58:44 -0400

    Added kv ttl constant

--- utility.js
+++ utility.js
@@ -29,3 +29,4 @@ export function getCookies(request) {
 
 export const cookieTTL = 5 * 60 * 1000; // 5 minutes
 export const heartbeatLength = 1000 // 1 second
+export const kvTTL = {expirationTtl: 86400} // 1 day
