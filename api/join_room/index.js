a14e19fc5b6a566db35804c50a938947d8852005
Maple Xu <maplexu2010@gmail.com>
2020-04-25 16:35:49 -0400

    Finished first version of join_room api code

--- api/join_room/index.js
+++ api/join_room/index.js
@@ -1,4 +1,5 @@
 import { FilterEvent } from '../../utility'
+import * as utility from '../../utility'
 
 addEventListener('fetch', event => { 
     FilterEvent(event, '/api/ready', handleReady); 
@@ -12,9 +13,7 @@ addEventListener('fetch', event => {
  *   Returns empty body when successful
  */
 async function handleReady(request) {
-  return new Response(
-    JSON.stringify({}), 
-    { headers: { 'content-type': 'application/json'}});
+    return handleReadyButtonHelper(request, 1)
 }
 
 /**
@@ -23,9 +22,16 @@ async function handleReady(request) {
  *   Returns empty body when successful
  */
 async function handleNotReady(request) {
-  return new Response(
-    JSON.stringify({}), 
-    { headers: { 'content-type': 'application/json'}});
+    return handleReadyButtonHelper(request, 0)
+}
+
+function handleReadyButtonHelper(request, newVal) {
+    const cookies = getCookies(request)
+    const position = await GAME.get(cookies.user + '-position')
+    GAME.put(cookies.game.concat('-', position, '-ready'), newVal, utility.kvTTL)
+    return new Response(
+        JSON.stringify({}), 
+        { headers: { 'content-type': 'application/json'}});
 }
 
 /**
@@ -38,10 +44,36 @@ async function handleNotReady(request) {
  *     players: []
  *       List is size 3, indicating players left, opposite and right
  *       Each element will be player ID, or null if player has not joined
+ *     ready: true/false
  *   }
  */
 async function handleHeartbeat(request) {
-  return new Response(
-    JSON.stringify({}), 
-    { headers: { 'content-type': 'application/json'}});
+    const cookies = getCookies(request)
+    getKey = (pos, command) => cookies.game.concat('-', pos, '-', command)
+    [GAME.get(cookies.game.concat)]
+    promises = []
+    for (i = 0; i < 4; ++i) { promises.push(GAME.get(getKey(i, 'user'))) }
+    for (i = 0; i < 4; ++i) { promises.push(GAME.get(getKey(i, 'ready'))) }
+    const values = await Promise.all(promises);
+    const myPosition = values.indexOf(cookies.user);
+    const now = Date.now()
+    GAME.put(getKey(myPosition, 'heart'), now, utility.KvTTL)
+    players = promises.slice(myPosition + 1, 4)
+    if (myPosition !== 3) {
+        players.push(...promises.slice(myPosition))
+    }
+    go = (promises[4] === '1' && promises[5] === '1' && promises[6] === '1' && promises[7] === '1')
+    if (go) {
+        // Grab heartbeats to heck that nobody disconnected
+        heartPromises = []
+        for (i = 0; i < 4; ++i) { 
+            if (i !== myPosition) heartPromises.push(GAME.get(getKey(i, 'heart')))
+            else heartPromises.push(null)
+        }
+        go = await Promise.all(heartPromises).then(heartbeats => 
+            heartbeats.filter(beat => now - beat > utility.heartbeatLength * consideredDead))
+    }
+    return new Response(
+        JSON.stringify({go, players, ready: promises.myPosition === 'true'}), 
+        { headers: { 'content-type': 'application/json'}});
 }
--- utility.js
+++ utility.js
@@ -29,4 +29,5 @@ export function getCookies(request) {
 
 export const cookieTTL = 5 * 60 * 1000; // 5 minutes
 export const heartbeatLength = 1000 // 1 second
+export const consideredDead = 5 // Number of heartbeats missed
 export const kvTTL = {expirationTtl: 86400} // 1 day
