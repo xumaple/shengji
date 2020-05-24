8d92801fba3a930698257f888dee33ab738f9d68
Daniel Lee <junwon@umich.edu>
2020-05-24 16:07:39 -0400

    css

--- source/api/jsx/room.jsx
+++ source/api/jsx/room.jsx
@@ -39,7 +39,7 @@ class Room extends React.Component {
 
     componentDidMount() {
         // console.log("heartbeat called")
-        setTimeout(this.heartbeat, 0);
+        // setTimeout(this.heartbeat, 0);
         window.addEventListener('beforeunload', (e) => {
             e.preventDefault();
             fetch(this.props.url.concat('leave/'));
@@ -61,7 +61,11 @@ class Room extends React.Component {
                 <WaitingRoom url={this.props.url} db={this.props.db.child('players')} />
             </div>
         }
-        return <div>game</div>
+        return <div className='game'>game!!!!!!!!!!!!!
+            <div className='arena'>
+                <Hand db={this.props.db.child('hand')}/>
+            </div>
+        </div>
   }
 }
 
@@ -230,6 +234,80 @@ class WaitingRoom extends React.Component {
                     <button onClick={this.handleClick}>{ready === false ? 'Ready' : 'Cancel'}</button>
                     </span> : ''}</div>
             })}
+        </div>;
+    }
+}
+
+class Hand extends React.Component {
+    constructor(props) {
+        super(props);
+
+        let ids = ['1S', 'JH', '7H', '5H', '1C', 'KC', 'QC', '8C', '6D', '2D', '2D']
+        const cards = [];
+        for (let i = 0; i < ids.length; i++) {
+            cards.push({
+                idx: i,
+                id: ids[i],
+                select: false,
+            });
+        }
+        this.state = { cards: cards, played_cards: [] };
+
+        this.handleClick = this.handleClick.bind(this);
+        this.selectCard = this.selectCard.bind(this);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        console.log('playing');
+        let { cards, played_cards } = this.state;
+        played_cards = cards.filter((card) => card.select);
+        cards = cards.filter((card) => !card.select);
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].idx = i;
+        }
+        this.setState({ cards: cards, played_cards: played_cards });
+    }
+
+    selectCard(card_idx) {
+        const { cards } = this.state;
+        cards[card_idx].select = !cards[card_idx].select;
+        this.setState({ cards: cards, played_cards: [] });
+    }
+
+    render() {
+        console.log('rendering hand');
+        let card_list = this.state.cards.map((card) => <Card key={card.id} card={card.id} idx={card.idx} select_card={(card_idx) => this.selectCard(card_idx)} />);
+        let played_card_list = this.state.played_cards.map((card) => <img key={card.idx} className='play-card' src={['/static/cards/', card.id, '.png'].join('')} />);
+        return <div className='hand'>
+            <div className='played-hand'>
+                {played_card_list}
+            </div>
+            {card_list}
+            <button className='play-button' onClick={this.handleClick}>Play</button>
+        </div>;
+    }
+}
+
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {cardImgUrl: ['/static/cards/', props.card, '.png'].join(''), selected: false};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        const { cardImgUrl, selected } = this.state;
+        this.setState({cardImgUrl, selected: !selected});
+    }
+
+    render() {
+        console.log('rendering card');
+        return <div>
+            <img className={this.state.selected ? ['card-selected', 'select-card'].join(' ') : ['card', 'unselect-card'].join(' ')} src={this.state.cardImgUrl} onClick={(e) => {this.handleClick(e); this.props.select_card(this.props.idx);}} role="button" tabIndex="0" />
         </div>;
     }
 }
